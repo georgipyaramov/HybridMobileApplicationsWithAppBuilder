9db31f2ab206d2acde9c986d6c1401d646577fac
themagicis <themagicis777@gmail.com>
2014-09-28 19:15:49 +0300

    get coordinates of market done

--- CheepVodkaApp/CheepVodkaApp/scripts/view-models/addMarket.js
+++ CheepVodkaApp/CheepVodkaApp/scripts/view-models/addMarket.js
@@ -4,10 +4,40 @@ app.viewmodels = app.viewmodels || {};
 (function (scope) {
     'use strict';
 
+    function getCoords(callback) {
+        var lat = 0,
+            long = 0;
+        var geolocationSuccess = function(position) {
+            console.log(position)
+            callback({
+                lat: position.coords.latitude,
+                long: position.coords.longitude,
+            });
+        }
+
+        var geolocationError = function (position) {
+            console.log("error")
+        }
+
+        navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
+    }
+
     scope.addMarket = kendo.observable({
-            name: '',
-            saveMarket: function () {
-                window.data.markets.addMarket(this.get('name'), 2, 3);
-            }
-        });
+        name: '',
+        lat: 0,
+        long: 0,
+        getCoords: function () {
+            var data = getCoords(function (data) {
+                scope.addMarket.set('lat', data.lat);
+                scope.addMarket.set('long', data.long);
+                scope.addMarket.set('coordsReady', true);
+                scope.addMarket.set('showSearching', false);
+            });
+        },
+        coordsReady: false,
+        showSearching: true,
+        saveMarket: function () {
+            window.data.markets.addMarket(this.get('name'), this.get('lat'), this.get('long'));
+        }
+    });
 }(app.viewmodels));
